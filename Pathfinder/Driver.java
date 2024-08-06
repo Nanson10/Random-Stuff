2f292a32a796c6928ca2715f0524c1d9ffed349b
Nanson Chen <buyingsaucer2@gmail.com>
2024-08-06 00:43:54 -0400

    Pathfinder v1 complete

--- Pathfinder/Driver.java
+++ Pathfinder/Driver.java
@@ -1,10 +1,22 @@
 package Pathfinder;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-        Reader reader = new Reader(new File("Pathfinder/map1.dat"));
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a file number: ");
+        number = sc.nextInt();
+        File file = new File("Pathfinder/map" + number + ".dat");
+        while (!file.exists()) {
+            System.out.print("Invalid file number, please enter a new one: ");
+            number = sc.nextInt();
+            file = new File("Pathfinder/map" + number + ".dat");
+        }
+        sc.close();
+        Reader reader = new Reader(file);
         Pather.printPath(reader);
     }
 }
